97e17b2d1bcc6f1c1d8a2c50326bb4f1e7ea35f3
CuteWisp <sweatpotato13@gmail.com>
2020-06-16 09:38:17 +0900

    Make Embed SwiftUI View into the storyboard

--- CurrencyConverterWidget/TodayViewController.swift
+++ CurrencyConverterWidget/TodayViewController.swift
@@ -6,23 +6,23 @@
 //  Copyright © 2020 BTU. All rights reserved.
 //
 
+import SwiftUI
 import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Allow the today widget to be expanded or contracted.
+    
     }
         
+    @IBSegueAction func addSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: WidgetView())
+    }
+    
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
         completionHandler(NCUpdateResult.newData)
     }
     
---
+++ CurrencyConverterWidget/WidgetView.swift
@@ -0,0 +1,15 @@
+//
+//  WisgetView.swift
+//  CurrencyConverterWidget
+//
+//  Created by BTU on 2020/06/16.
+//  Copyright © 2020 BTU. All rights reserved.
+//
+
+import SwiftUI
+
+struct WidgetView : View {
+    var body: some View {
+        Text("This is a swiftUI view 👋")
+    }
+}
